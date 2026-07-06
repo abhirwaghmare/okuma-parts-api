4a3ddca0f68fead8dbacb9461d3f62f01b2b6d8a
Mohammad, Lal <llal+deloitte@deloitte.com>
2026-07-06 16:13:18 +0530

    OCI61-58 sub header changes

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -7,6 +7,7 @@ const auth = require('./auth');
 const webhooks = require('./webhooks');
 const partsBook = require('./parts-book');
 const dealers = require('./dealers');
+const customer = require('./customer');
 
 const router = Router();
 
@@ -15,6 +16,7 @@ router.use('/api/products', products);
 router.use('/auth', auth);
 router.use('/webhooks', webhooks);
 router.use('/', dealers);
+router.use('/', customer);
 router.use('/', partsBook);
 
 module.exports = router;
